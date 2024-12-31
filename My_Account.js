798ea50bc938aa3189953c241a4e9a06f38afaa2
ofri applebaum <ofriap@post.bgu.ac.il>
2024-12-31 13:11:07 +0200

    Create My_Account.js

---
+++ My_Account.js
@@ -0,0 +1,15 @@
+  function loadUserDetails() {
+            const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+            if (userDetails) {
+                document.getElementById("display_FirstName").innerText = userDetails.firstName;
+                document.getElementById("display_LastName").innerText = userDetails.lastName;
+                document.getElementById("display_BirthDate").innerText = userDetails.birthDate;
+                document.getElementById("display_Email").innerText = userDetails.email;
+                document.getElementById("display_Phone").innerText = userDetails.phone;
+            } else {
+                alert("לא נמצאו פרטים");
+            }
+        }
+
+        // טוען את פרטי המשתמש בעת טעינת הדף
+        window.onload = loadUserDetails;
