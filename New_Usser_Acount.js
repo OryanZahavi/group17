8c6c79df45ffa0ff4df685dad37edad750b44b16
ofri applebaum <ofriap@post.bgu.ac.il>
2025-01-06 10:24:41 +0200

    Update New_Usser_Acount.js

--- New_Usser_Acount.js
+++ New_Usser_Acount.js
@@ -1,14 +1,14 @@
- function saveUserDetails() {
-        const userDetails = {
-            firstName: document.getElementById("user_FirstName").value,
-            lastName: document.getElementById("user_LastName").value,
-            birthDate: document.getElementById("user_Date").value,
-            email: document.getElementById("user_Email").value,
-            phone: document.getElementById("user_Phone").value,
-            password: document.getElementById("user_Password").value,
-        };
+function saveUserDetails() {
+    const userDetails = {
+        firstName: document.getElementById("user_FirstName").value,
+        lastName: document.getElementById("user_LastName").value,
+        birthDate: document.getElementById("user_Date").value,
+        email: document.getElementById("user_Email").value,
+        phone: document.getElementById("user_Phone").value,
+        password: document.getElementById("user_Password").value,
+    };
 
-        localStorage.setItem("userDetails", JSON.stringify(userDetails));
-        alert("הפרטים נשמרו בהצלחה!");
-        window.location.href = "My_Account.html"; // מעבר לדף הפרטים
-    }
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    alert("הפרטים נשמרו בהצלחה!");
+    window.location.href = "My_Account.html"; // מעבר לדף הפרטים
+}
